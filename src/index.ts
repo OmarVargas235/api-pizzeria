44fb4f406b2e527c052b2a11c047d85f923c90ff
Omar Vargas <omar.vargas@valorpercibido.com>
2026-06-04 19:42:21 -0500

    chore(project): bootstrap project foundation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import app from "./app.js";
 import { env } from "./config/env.js";
 
 app.listen(env.PORT, () => {
-    // eslint-disable-next-line no-console
     console.log(`🚀 Server running on http://localhost:${env.PORT}`);
 });
