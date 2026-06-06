eef06e6e41e923c1052376531862ecaa91efbd88
Omar Vargas <55988968+OmarVargas235@users.noreply.github.com>
2026-06-05 19:18:06 -0500

    chore: configure env variables and add docker setup (#2)
    Co-authored-by: Omar Vargas <ov1356272@gmail.com>

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,10 @@ dotenv.config();
 
 const envSchema = z.object({
     PORT: z.coerce.number(),
+    DATABASE_URL: z.string(),
+    JWT_SECRET: z.string(),
+    EMAIL_USER: z.string(),
+    EMAIL_PASSWORD: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
